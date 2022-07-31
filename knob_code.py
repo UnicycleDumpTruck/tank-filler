091269b7309ed744fd84ddb9f967a3e5310101df
UnicycleDumpTruck <junk@jeffhighsmith.com>
2022-07-31 17:22:44 -0400

    List of Knob objects, resolution adjustment

--- knob_code.py
+++ knob_code.py
@@ -1,43 +1,50 @@
 # SPDX-FileCopyrightText: 2021 John Furcean
 # SPDX-License-Identifier: MIT
 
-"""I2C rotary encoder simple test example."""
+"""I2C rotary encoders in array of Knobs."""
 
 import board
 from adafruit_seesaw import seesaw, rotaryio, digitalio
 
-# For use with the STEMMA connector on QT Py RP2040
-# import busio
-# i2c = busio.I2C(board.SCL1, board.SDA1)
-# seesaw = seesaw.Seesaw(i2c, 0x36)
+RESOLUTION = 20 # Only change on every nth position.
 
-seesaw = seesaw.Seesaw(board.I2C(), addr=0x36)
+class Knob():
+    def __init__(self, seesaw):
+        self.seesaw = seesaw
+        seesaw_product = (self.seesaw.get_version() >> 16) & 0xFFFF
+        print("Found product {}".format(seesaw_product))
+        if seesaw_product != 4991:
+            print("Wrong firmware loaded?  Expected 4991")
 
-seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
-print("Found product {}".format(seesaw_product))
-if seesaw_product != 4991:
-    print("Wrong firmware loaded?  Expected 4991")
+        self.button = digitalio.DigitalIO(seesaw, 24)
+        self.button_held = False
+        self.encoder = rotaryio.IncrementalEncoder(self.seesaw)
+        self.last_position = 0
 
-seesaw.pin_mode(24, seesaw.INPUT_PULLUP)
-button = digitalio.DigitalIO(seesaw, 24)
-button_held = False
+    def update(self):
+        position = -self.encoder.position
+        if position > self.last_position:
+            self.last_position = position
+            print("Position increased to: {}".format(position))
+            if position % RESOLUTION == 0:
+                print("ADD "*15)
+        elif position < self.last_position:
+            self.last_position = position
+            print("Position decreased to: {}".format(position))
+            if position % RESOLUTION == 0:
+                print("REMOVE "*15)
+        if not self.button.value and not self.button_held:
+            self.button_held = True
+            print("Button pressed")
+        if self.button.value and self.button_held:
+            self.button_held = False
+            print("Button released")
 
-encoder = rotaryio.IncrementalEncoder(seesaw)
-last_position = None
+knobs = []
+knobs.append(Knob(seesaw.Seesaw(board.I2C(), addr=0x36)))
+
 
 while True:
 
-    # negate the position to make clockwise rotation positive
-    position = -encoder.position
-
-    if position != last_position:
-        last_position = position
-        print("Position: {}".format(position))
-
-    if not button.value and not button_held:
-        button_held = True
-        print("Button pressed")
-
-    if button.value and button_held:
-        button_held = False
-        print("Button released")
+    for knob in knobs:
+        knob.update()
